261ff016836c502cd68818eee654ef26647fe0f4
Kiminori Mizushino <k_mizushino@ewings.biz>
2025-09-30 13:07:14 +0900

    fix(config): Set default port to 3000 if not specified in environment

--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -4,7 +4,7 @@ export default {
   nodeResolve: true,
   watch: true,
   hostname: '0.0.0.0',
-  port: parseInt(process.env.PORT, 10),
+  port: parseInt(process.env.PORT, 10) || 3000,
   files: ['src/**/*.ts'],
   plugins: [esbuildPlugin({ts: true, tsconfig: 'tsconfig.json'})],
 };
